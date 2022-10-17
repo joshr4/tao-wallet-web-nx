8d9f29ec60b8bc55bfca29de7d1641f5bf4b7b66
Josh <joshremaley@gmail.com>
2022-10-17 17:13:54 -0500

    add node server app

---
+++ apps/tao-wallet-node/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'tao-wallet-node',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/tao-wallet-node',
+};
